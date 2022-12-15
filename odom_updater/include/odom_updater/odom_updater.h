231a1ccdfb70f230d68f584c1c86bd64c35fd1f6
Kshitij Karnawat <kshitijkarnawat009@gmail.com>
2022-12-14 21:59:15 -0500

    Add files via upload

--- odom_updater/include/odom_updater/odom_updater.h
+++ odom_updater/include/odom_updater/odom_updater.h
@@ -2,31 +2,44 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include <nav_msgs/msg/odometry.hpp>
-#include "tf2/LinearMath/Quaternion.h"
 #include "geometry_msgs/msg/pose.hpp"
 #include "tf2_ros/transform_broadcaster.h"
 #include <string>
+#include<sstream>
 
 class OdomBroadcaster : public rclcpp::Node
 {
 public:
     OdomBroadcaster(std::string node_name) : Node(node_name)
     {
+        
         // Initialize the transform broadcaster
         tf_broadcaster =
             std::make_shared<tf2_ros::TransformBroadcaster>(this);
 
+        std::ostringstream stream;
+        stream << "/robot1/odom";
+
+        std::string topic_name = stream.str();
+
+        odom_subscriber = this->create_subscription<nav_msgs::msg::Odometry>(
+        topic_name, 10,
+        std::bind(&OdomBroadcaster::odom_callback, this, std::placeholders::_1));
+
         // Create a timer
-        timer = this->create_wall_timer(std::chrono::milliseconds((int)(1000.0 / 1.0)),
-                                          std::bind(&OdomBroadcaster::timer_callback, this));
+        // timer = this->create_wall_timer(std::chrono::milliseconds((int)(1000.0 / 1.0)),
+        //                                   std::bind(&OdomBroadcaster::timer_callback, this));
     }
 
 private:
     // attributes
     rclcpp::TimerBase::SharedPtr timer;
-    std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
 
+    std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
+    
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscriber;
+    
     // methods
-    void broadcast_odom();
-    void timer_callback();
+    // void timer_callback();
+    void odom_callback(const std::shared_ptr<nav_msgs::msg::Odometry> msg);
 };
